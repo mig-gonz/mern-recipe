99345e5371bb8fc90c12732a21140bb66e7cef3b
mig-gonz <mig256@outlook.com>
2023-05-23 13:03:13 -0700

    deleted vercel file

--- api/server.js
+++ api/server.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../mern-recipe/build")));
 
 // CONTROLLERS
 const recipesController = require("./controllers/recipes_controller");
@@ -18,4 +19,8 @@ app.use("/api/recipes", recipesController);
 // LISTEN
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../mern-recipe/build/index.html"));
 });
